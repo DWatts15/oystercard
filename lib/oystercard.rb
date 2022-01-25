dd0c311ce25134822566dcf6474b6256dc02c6c1
Daniel Watts <d.watts15@outlook.com>
2022-01-25 16:10:35 +0000

    Up to Challenge 11.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,19 +1,38 @@
 # Makers Week 4 challenge: Oyster Card
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey, :MIN_FARE
 
   MAX_BALANCE = 90
+  MIN_BALANCE = 1
+  MIN_FARE = 1
   
   def initialize(balance = 0)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
-    limit_exceeded = "Max limit is £90."
-    
-    fail "test" unless @balance + amount <= MAX_BALANCE
+    fail "Max balance exceeded" unless @balance + amount <= MAX_BALANCE
     @balance += amount
+  end
 
+  def in_journey?
+    @in_journey
+  end
 
+  def touch_in
+    fail "insufficient balance" if balance < MIN_BALANCE
+    @in_journey = true 
+  end
+
+  def touch_out
+    deduct(MIN_FARE)
+    @in_journey = false
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -26,6 +26,43 @@ describe Oystercard do
   it {expect(oystercard).to respond_to(:top_up)}
 
   it "fails if the top_up increases balance beyond the limit" do
-    expect {oystercard.top_up(1999)}.to raise_error 'test'
+    expect {oystercard.top_up(1999)}.to raise_error 'Max balance exceeded'
   end
+
+  it "should deduct a fare from the card" do
+    oystercard.top_up(50)
+    oystercard.touch_out
+    expect(oystercard.balance).to eq(49)
+  end
+
+  it "is initially not in journey" do
+    expect(oystercard).not_to be_in_journey
+  end
+
+  it "can touch in" do
+    oystercard.top_up(5)
+    oystercard.touch_in
+    expect(oystercard).to be_in_journey
+  end
+
+  it "can touch out" do
+    oystercard.touch_out
+    expect(oystercard).not_to be_in_journey
+    expect { oystercard.touch_out }.to change { oystercard.balance }.by (-Oystercard::MIN_FARE)
+  end
+
+  it 'will not touch in if under minimum balance' do
+    expect{ oystercard.touch_in }.to raise_error "insufficient balance"
+  end
+
+  # it 'deducts a minimum fare from card when used' do
+  #   expect { oystercard.touch_out }.to change { oystercard.balance }.by (-Oystercard::MIN_FARE)
+  # end
+
+  # let(:station){ double :station }
+
+  # it "stores the entry station" do
+  #   oystercard.touch_in
+  #   expect(oystercard.entry_station).to eq station
+  # end
 end
