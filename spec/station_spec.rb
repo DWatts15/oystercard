5d5f542657b0c6d2fdc0c2af65bba0ecf2e27074
Daniel Watts <d.watts15@outlook.com>
2022-01-27 15:15:48 +0000

    Refactored station_spec.rb file.

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,15 +1,13 @@
 require 'station'
 
 describe Station do
-  let(:station) {Station.new}
+  let(:station) {Station.new("city", "1")}
 
   it "has an station name property upon instantiation" do
-    stat = Station.new("city", "1")
-    expect(stat.station_info[:name]).to eq("city")
+    expect(station.station_info[:name]).to eq("city")
   end
 
   it "has a zone property upon instantiation" do
-    stat = Station.new("city", "1")
-    expect(stat.station_info[:zone]).to eq("1")
+    expect(station.station_info[:zone]).to eq("1")
   end
 end
